311f26c61be1d755853ef6c5e1bd1e3a7bdbad01
Livingroom507 <64347895+Livingroom507@users.noreply.github.com>
2025-11-09 23:05:56 -0500

    Updated the profile-details API to include user role information.

--- functions/api/admin/profile-details.js
+++ functions/api/admin/profile-details.js
@@ -12,11 +12,11 @@ export async function onRequestGet(context) {
     const query = `
       SELECT 
           p.*, 
-          m.module1_score, m.module2_score, m.module3_score, m.total_score, m.knowledge_level
+          m.score AS module3_score
       FROM 
           PlacementProfiles p
       LEFT JOIN 
-          QuizResults m ON p.email = m.email
+          module3_results m ON p.email = m.email
       WHERE 
           p.email = ?;
     `;
